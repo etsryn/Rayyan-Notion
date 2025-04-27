74c93a3dfd369f1bc3cd2e8f19f3f55df6669877
Rayyan Ashraf <ryshashraf@gmail.com>
2025-04-28 00:42:47 +0530

    SPA-C

--- src/components/Landing-Page/landingPage.jsx
+++ src/components/Landing-Page/landingPage.jsx
@@ -429,7 +429,7 @@ const LandingPage = () => {
             >
               <SiLinkedin />
               &nbsp;Connect
-            </a> | v1.0.0
+            </a> | v3.1.1-1
           </footer>
         )}
       </aside>
